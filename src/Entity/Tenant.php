e6071a0444c0a37b39f715e35a25b01ecc629c74
Christian Linde <info@christian-linde.de>
2024-08-15 10:22:31 +0200

    change tenant to interface

--- src/Entity/Tenant.php
+++ src/Entity/Tenant.php
@@ -8,124 +8,23 @@ use Symfony\Component\Serializer\Attribute\Ignore;
 use Symfony\Component\Validator\Constraints\Unique;
 
 /**
- * Superclass for the tenant entity
+ * Interface for the tenant entity
  * Contains the required data to connect to the tenant's datasource
  */
-#[MappedSuperclass]
-class Tenant {
+interface Tenant {
 
-    #[Unique]
-    #[ORM\Column(length: 50)]
-    private ?string $identifier = null;
+    public function getIdentifier(): ?string;
 
-    #[ORM\Column(nullable: false)]
-    private bool $enabled = false;
+    public function isEnabled(): bool;
 
-    #[Ignore]
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $serverIp = null;
+    public function getServerIp(): ?string;
 
-    #[Ignore]
-    #[ORM\Column(nullable: true)]
-    private ?int $databasePort = null;
+    public function getDatabasePort(): ?int;
 
-    #[Ignore]
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $databaseName = null;
+    public function getDatabaseName(): ?string;
 
-    #[Ignore]
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $databaseUser = null;
+    public function getDatabaseUser(): ?string;
 
-    #[Ignore]
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $databasePassword = null;
-
-    public function getIdentifier(): ?string
-    {
-        return $this->identifier;
-    }
-
-    public function setIdentifier(?string $identifier): self
-    {
-        $this->identifier = $identifier;
-        return $this;
-    }
-
-    public function isEnabled(): bool
-    {
-        return $this->enabled;
-    }
-
-    public function setEnabled(bool $enabled): self
-    {
-        $this->enabled = $enabled;
-        return $this;
-    }
-
-    #[Ignore]
-    public function getServerIp(): ?string
-    {
-        return $this->serverIp;
-    }
-
-    #[Ignore]
-    public function setServerIp(?string $serverIp): self
-    {
-        $this->serverIp = $serverIp;
-        return $this;
-    }
-
-    #[Ignore]
-    public function getDatabasePort(): ?int
-    {
-        return $this->databasePort;
-    }
-
-    #[Ignore]
-    public function setDatabasePort(?int $databasePort): self
-    {
-        $this->databasePort = $databasePort;
-        return $this;
-    }
-
-    #[Ignore]
-    public function getDatabaseName(): ?string
-    {
-        return $this->databaseName;
-    }
-
-    #[Ignore]
-    public function setDatabaseName(?string $databaseName): self
-    {
-        $this->databaseName = $databaseName;
-        return $this;
-    }
-
-    #[Ignore]
-    public function getDatabaseUser(): ?string
-    {
-        return $this->databaseUser;
-    }
-
-    #[Ignore]
-    public function setDatabaseUser(?string $databaseUser): self
-    {
-        $this->databaseUser = $databaseUser;
-        return $this;
-    }
-
-    #[Ignore]
-    public function getDatabasePassword(): ?string
-    {
-        return $this->databasePassword;
-    }
-
-    #[Ignore]
-    public function setDatabasePassword(?string $databasePassword): self
-    {
-        $this->databasePassword = $databasePassword;
-        return $this;
-    }
+    public function getDatabasePassword(): ?string;
 
 }
